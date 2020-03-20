21e5661d5a389b80ba6fb1ae0c158ec78bffc918
venussmak <51063426+venussmak@users.noreply.github.com>
2020-03-19 23:28:58 -0700

    Add files via upload
    java doc added

---
+++ SelfCreation.java
@@ -0,0 +1,51 @@
+
+/**
+ *  @author Yunjing Mai
+ * @author Xinfang Zhang
+ * CIS 36B, Final Project
+**/
+
+public class SelfCreation extends BasicSalad {
+	private final String NAME = "Self made";
+
+	/**
+	 * Default constructor for SelfCreation. 
+	 * call superclass
+	 */
+	public SelfCreation() {
+		super();
+	}
+
+	/**
+	 * One argument constructor for SelfCreation.
+	 * call superclass with size
+	 */
+	public SelfCreation(String size) {
+		super(size);
+	}
+
+	/**
+	 * Calculate the total calories for all ingredients within a salad
+	 * @return total calories
+	 */
+	@Override 
+	public int totalCalories() {
+		int total = 0;
+		for (int i = 0; i < dishIngredient.size(); i++) {
+			total += dishIngredient.get(i).getNumber() * dishIngredient.get(i).getCalories();
+		}
+		return total;
+	}
+
+	/**
+	 * Call printIngredient() from BasicSalad
+	 * Print the name and the total calories of the salad 
+	 */
+	@Override
+	public String toString() {
+		return "\nYour choice: " + NAME + printIngredient() + "\nCalorie: " + this.totalCalories() + " calories"
+				+ super.toString();
+	}
+
+}
+
